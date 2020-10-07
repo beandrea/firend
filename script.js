1ad6d2be03a16f0bfba55d2968c5a8ee2b5a28aa
Izzy <65364798+queenIsabella99@users.noreply.github.com>
2020-10-06 22:37:59 -0400

    commit 99

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
     "w", "x", "y", "z"];
+
 var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
 var alphaNum = [alpha, num];
 
 function generatePassword() {
     var securePas = "";
     for (var i = 0; i < 10; i++) {
-        var source = alphaNum[Math.floor(Math.random() * 2)];
-        if (source == alpha) {
+        var let = alphaNum[Math.floor(Math.random() * 2)];
+        if (let == alpha) {
             securePas += alpha[Math.floor(Math.random() * alpha.length)];
-        } else if(source == num){
+        } else {
             securePas += num[Math.floor(Math.random() * num.length)];
         }
     }
+    return securePas;
 }
 
 // Write password to the #password input
