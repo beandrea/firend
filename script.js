a2bd9759a6e71cf2ecd78e21f795266f7b54f9e2
Izzy <65364798+queenIsabella99@users.noreply.github.com>
2020-10-15 21:26:08 -0400

    commit 100

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-    "w", "x", "y", "z"];
-
-var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-var alphaNum = [alpha, num];
-
 function generatePassword() {
     var securePas = "";
     for (var i = 0; i < 10; i++) {
-        var let = alphaNum[Math.floor(Math.random() * 2)];
-        if (let == alpha) {
-            securePas += alpha[Math.floor(Math.random() * alpha.length)];
-        } else {
-            securePas += num[Math.floor(Math.random() * num.length)];
-        }
+        securePas += String.fromCharCode(Math.floor(35 + Math.random() * 91));
     }
+    console.log(securePas);
     return securePas;
 }
 
